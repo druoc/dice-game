73571f445b93c172e74d4ec4438780d1194d91a0
Dave <d.ruocco1987@googlemail.com>
2023-10-30 13:44:17 +0000

    Initial commit, sets up project and Dice class

---
+++ Dice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceGame
+{
+    internal class Dice
+    {
+        private Random randomNumber = new Random();
+        private int diceNumber;
+
+        public Dice()
+        {
+            diceNumber = randomNumber.Next(1, 6);
+        }
+
+        public int DiceRoll()
+        {
+            return diceNumber;
+        }
+    }
+}
