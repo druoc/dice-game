b7b5cc7f0579f2ccc7c266ba4bb32ea7b91b50e6
Dave <d.ruocco1987@googlemail.com>
2023-10-30 14:30:02 +0000

    Implements main game functionality

--- Program.cs
+++ Program.cs
@@ -1,10 +1,39 @@
-﻿namespace DiceGame
+﻿using System.Runtime.CompilerServices;
+
+namespace DiceGame
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            Dice dice = new Dice();
+            int diceNumber = dice.DiceRoll();
+            int amountOfGuesses = 1;
+            
+            Console.WriteLine("Dice rolled, try and guess which number:");
+            string userGuess = Console.ReadLine();
+            int userGuessConverted = Convert.ToInt32(userGuess);
+
+            do
+            {
+                if (userGuessConverted == diceNumber)
+                {
+                    Console.WriteLine($"Correct! The number was {diceNumber}");
+                    Console.WriteLine($"It took {amountOfGuesses} guesses.");
+                    amountOfGuesses = 3;
+                }
+                else
+                {
+                    Console.WriteLine($"Incorrect, try again! You have {3 - amountOfGuesses} guesses left.");
+                    amountOfGuesses++;
+                    userGuess = Console.ReadLine();
+                }
+            } while (amountOfGuesses < 3);
+
+            if (amountOfGuesses == 3)
+            {
+                Console.WriteLine("You lose! better luck next time!");
+            }
+    }
     }
 }
